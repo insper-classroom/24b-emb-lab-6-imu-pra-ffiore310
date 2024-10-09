9900b3bf04fd4caebafe7d1a45aee8bac87a5352
ffiore310 <fernandoof@al.insper.edu.br>
2024-10-09 14:25:00 +0000

    commit inicial, pois esqueci de dar

--- main/main.c
+++ main/main.c
@@ -12,18 +12,24 @@
 
 #include <Fusion.h>
 
+#define UART_ID uart0
+#define BAUD_RATE 115200
+
 const int MPU_ADDRESS = 0x68;
 const int I2C_SDA_GPIO = 4;
 const int I2C_SCL_GPIO = 5;
 
+const int tx = 16;
+const int rx = 17;
+
 static void mpu6050_reset() {
     // Two byte reset. First byte register, second byte data
     // There are a load more options to set up the device in different ways that could be added here
-    uint8_t buf[] = {0x6B, 0x00};
+    uint8_t buf[] = { 0x6B, 0x00 };
     i2c_write_blocking(i2c_default, MPU_ADDRESS, buf, 2, false);
 }
 
-static void mpu6050_read_raw(int16_t accel[3], int16_t gyro[3], int16_t *temp) {
+static void mpu6050_read_raw(int16_t accel[3], int16_t gyro[3], int16_t* temp) {
     // For this particular device, we send the device the register we want to read
     // first, then subsequently read from the device. The register is auto incrementing
     // so we don't need to keep sending the register we want, just the first.
@@ -58,8 +64,12 @@ static void mpu6050_read_raw(int16_t accel[3], int16_t gyro[3], int16_t *temp) {
     *temp = buffer[0] << 8 | buffer[1];
 }
 
-void mpu6050_task(void *p) {
+void mpu6050_task(void* p) {
     i2c_init(i2c_default, 400 * 1000);
+    uart_init(UART_ID, BAUD_RATE);
+
+    gpio_set_function(tx, GPIO_FUNC_UART);
+    gpio_set_function(rx, GPIO_FUNC_UART);
     gpio_set_function(I2C_SDA_GPIO, GPIO_FUNC_I2C);
     gpio_set_function(I2C_SCL_GPIO, GPIO_FUNC_I2C);
     gpio_pull_up(I2C_SDA_GPIO);
@@ -68,11 +78,41 @@ void mpu6050_task(void *p) {
     mpu6050_reset();
     int16_t acceleration[3], gyro[3], temp;
 
-    while(1) {
+    FusionAhrs ahrs;
+    FusionAhrsInitialise(&ahrs);
+
+    while (1) {
         mpu6050_read_raw(acceleration, gyro, &temp);
-        printf("Acc. X = %d, Y = %d, Z = %d\n", acceleration[0], acceleration[1], acceleration[2]);
-        printf("Gyro. X = %d, Y = %d, Z = %d\n", gyro[0], gyro[1], gyro[2]);
-        printf("Temp. = %f\n", (temp / 340.0) + 36.53);
+        // printf("Acc. X = %d, Y = %d, Z = %d\n", acceleration[0], acceleration[1], acceleration[2]);
+        // printf("Gyro. X = %d, Y = %d, Z = %d\n", gyro[0], gyro[1], gyro[2]);
+        // printf("Temp. = %f\n", (temp / 340.0) + 36.53);
+
+        FusionVector gyroscope = {
+            .axis.x = gyro[0] / 131.0f, // Conversão para graus/s
+            .axis.y = gyro[1] / 131.0f,
+            .axis.z = gyro[2] / 131.0f,
+        };
+
+        FusionVector accelerometer = {
+            .axis.x = acceleration[0] / 16384.0f, // Conversão para g
+            .axis.y = acceleration[1] / 16384.0f,
+            .axis.z = acceleration[2] / 16384.0f,
+        };
+
+        FusionAhrsUpdateNoMagnetometer(&ahrs, gyroscope, accelerometer, 0.01);
+
+        const FusionEuler euler = FusionQuaternionToEuler(FusionAhrsGetQuaternion(&ahrs));
+
+        printf("Roll %0.1f, Pitch %0.1f, Yaw %0.1f\n", euler.angle.roll, euler.angle.pitch, euler.angle.yaw);
+
+        uint8_t mais_sig = (data.val >> 8) & 0xFF;
+        uint8_t menos_sig = data.val & 0xFF;
+        uint8_t eop = 0xFF;
+        uart_putc_raw(UART_ID, 0);
+        uart_putc_raw(UART_ID, mais_sig);
+        uart_putc_raw(UART_ID, menos_sig);
+        uart_putc_raw(UART_ID, eop);
+
 
         vTaskDelay(pdMS_TO_TICKS(10));
     }
